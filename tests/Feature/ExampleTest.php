4b3ba1211871c72058c7ce5dc9085fe5177e0026
Babs27 <lilbabs300@gmail.com>
2025-12-26 13:48:37 +0100

    Corrige test Feature pour utiliser /products

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,7 +11,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        // Tester une route qui existe réellement dans ton projet
+        $response = $this->get('/products');
 
         $response->assertStatus(200);
     }
